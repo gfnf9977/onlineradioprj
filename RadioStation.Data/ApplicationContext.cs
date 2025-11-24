f7e6cb5f5ee52da5affc15b912a3164d47323e39
gfnf9977 <145157618+gfnf9977@users.noreply.github.com>
2025-11-24 20:57:17 +0200

    reviewsdelete

--- RadioStation.Data/ApplicationContext.cs
+++ RadioStation.Data/ApplicationContext.cs
@@ -8,7 +8,6 @@ namespace OnlineRadioStation.Data
         public DbSet<User> Users { get; set; }
         public DbSet<RadioStationEntity> Stations { get; set; }
         public DbSet<Track> Tracks { get; set; }
-        public DbSet<Review> Reviews { get; set; }
         public DbSet<LikeDislike> LikesDislikes { get; set; }
         public DbSet<DjStream> Streams { get; set; }
         public DbSet<SavedStation> SavedStations { get; set; }
